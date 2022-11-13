86ea9120046983fd4f10fe56f244aa32216afccb
KanjiNaem <janik.jehmlich@gmail.com>
2022-11-13 19:42:12 +0100

    implement kSums solution

---
+++ kSums.ts
@@ -0,0 +1,39 @@
+const isSum = (sum: number, k: number): boolean => {
+  const numSet = createNumSet(sum, k);
+
+  if (subsetSum(numSet, numSet.length, sum)) {
+    // evil return true if true because return subsetSum doesnt work for some reason T _T
+    return true;
+  }
+  return false;
+};
+
+const createNumSet = (sum: number, exp: number): number[] => {
+  let numSet: number[] = [];
+  let i = 1;
+
+  while (i ** exp <= sum) {
+    numSet.push(i ** exp);
+    i++;
+  }
+
+  return numSet;
+};
+
+let tab = Array(2000)
+  .fill(0)
+  .map(() => Array(2000).fill(-1));
+
+function subsetSum(a: number[], n: number, sum: number): any {
+  if (sum == 0) return 1;
+  if (n <= 0) return 0;
+  if (tab[n - 1][sum] != -1) return tab[n - 1][sum];
+  if (a[n - 1] > sum) return (tab[n - 1][sum] = subsetSum(a, n - 1, sum));
+  return (tab[n - 1][sum] =
+    subsetSum(a, n - 1, sum) || subsetSum(a, n - 1, sum - a[n - 1]));
+}
+
+const testSum = 36;
+const testK = 3;
+
+console.log(isSum(testSum, testK));
