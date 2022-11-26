9d7c6222ea81cd022cb19422f797b4503aa9e314
KanjiNaem <janik.jehmlich@gmail.com>
2022-11-26 03:28:52 +0100

    minor changes

--- genLinkedList.ts
+++ genLinkedList.ts
@@ -1,7 +1,6 @@
 class ListNode<T> {
   public value: T;
   public next: ListNode<T> | null;
-
   constructor(value: T) {
     this.value = value;
     this.next = null;
@@ -18,7 +17,6 @@ class LinkedList<T> {
 
   private iterate(index: number): ListNode<T> | null {
     let currentNode = this.head;
-
     while (index > 1) {
       index--;
       if (currentNode?.next) {
@@ -27,27 +25,21 @@ class LinkedList<T> {
       }
       throw new Error("index out of range");
     }
-
     return currentNode;
   }
 
   public push(value: T) {
     const newNode = new ListNode<T>(value);
     this.size++;
-
     if (!this.head) {
       this.head = newNode;
-
       return;
     }
-
     let current = this.head;
-
     while (current.next) {
       current = current.next;
     }
     current.next = newNode;
-
     return;
   }
 
@@ -55,30 +47,24 @@ class LinkedList<T> {
     if (!this.head) {
       return;
     }
-
     if (this.size === 1) {
       this.head = null;
       this.size--;
       return;
     }
-
     let current = this.head;
     let prevNode: ListNode<T> = current;
-
     while (current.next) {
       prevNode = current;
       current = current.next;
     }
-
     prevNode.next = null;
-
     this.size--;
     return;
   }
 
   public print() {
     let currentNode = this.head;
-
     while (currentNode) {
       console.log(currentNode.value);
       currentNode = currentNode.next;
@@ -96,11 +82,9 @@ class LinkedList<T> {
   public insertAtIndex(index: number, value: T) {
     const newNode = new ListNode<T>(value);
     const prevNode = this.iterate(index - 1);
-
     if (prevNode === null) {
       throw new Error("index out of range");
     }
-
     newNode.next = prevNode?.next ?? null;
     prevNode.next = newNode;
     this.size++;
@@ -129,12 +113,10 @@ class LinkedList<T> {
   public collect(): T[] {
     const arr: T[] = [];
     let currentNode = this.head;
-
     while (currentNode !== null) {
       arr.push(currentNode.value);
       currentNode = currentNode.next;
     }
-
     return arr;
   }
 }
